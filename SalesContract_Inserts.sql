8d67f3816b3e19ca9a7dd43c11d8a5c40e4afeb5
Nachi4265 <hvirgen@my.yearupunited.org>
2025-11-21 19:10:25 -0500

    Sales contract inserts SQL

---
+++ SalesContract_Inserts.sql
@@ -0,0 +1,38 @@
+use cardealershipdb;
+
+-- Sales Contracts
+INSERT INTO salescontracts(VIN, contractType, salesTax, recordingFee, processingFee, financed)
+VALUES(65783647265378497, 'sales contracts', 4.75, 100.00,295.00,1);
+
+INSERT INTO salescontracts(VIN, contractType, salesTax, recordingFee, processingFee, financed)
+VALUES(65783647265378497, 'SALE', 900.00, 100.00, 295.00, 1);
+
+INSERT INTO salescontracts(VIN, contractType, salesTax, recordingFee, processingFee, financed)
+VALUES(10001234567890123, 'SALE', 1387.50, 100.00, 295.00, 0);
+
+INSERT INTO salescontracts(VIN, contractType, salesTax, recordingFee, processingFee, financed)
+VALUES(10002345678901234, 'SALE', 1650.00, 100.00, 295.00, 1);
+
+INSERT INTO salescontracts(VIN, contractType, salesTax, recordingFee, processingFee, financed)
+VALUES(10003456789012345, 'SALE', 1875.00, 100.00, 295.00, 1);
+
+INSERT INTO salescontracts(VIN, contractType, salesTax, recordingFee, processingFee, financed)
+VALUES(10004567890123456, 'SALE', 2850.00, 100.00, 295.00, 1);
+
+INSERT INTO salescontracts(VIN, contractType, salesTax, recordingFee, processingFee, financed)
+VALUES(10005678901234567, 'SALE', 2400.00, 100.00, 295.00, 0);
+
+INSERT INTO salescontracts(VIN, contractType, salesTax, recordingFee, processingFee, financed)
+VALUES(10006789012345678, 'SALE', 2137.50, 100.00, 295.00, 0);
+
+INSERT INTO salescontracts(VIN, contractType, salesTax, recordingFee, processingFee, financed)
+VALUES(10007890123456789, 'SALE', 1950.00, 100.00, 295.00, 1);
+
+INSERT INTO salescontracts(VIN, contractType, salesTax, recordingFee, processingFee, financed)
+VALUES(10008901234567890, 'SALE', 2212.50, 100.00, 295.00, 0);
+
+INSERT INTO salescontracts(VIN, contractType, salesTax, recordingFee, processingFee, financed)
+VALUES(10009012345678901, 'SALE', 862.50, 100.00, 295.00, 1);
+
+INSERT INTO salescontracts(VIN, contractType, salesTax, recordingFee, processingFee, financed)
+VALUES(10000123456789012, 'SALE', 2325.00, 100.00, 295.00, 1);
